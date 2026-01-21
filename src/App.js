7f32004a514d86be6e37a240960a6389c972da53
Aleen_Alammar <aleen1alammar@gmail.com>
2026-01-21 11:11:54 +0300

    ui issues

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default function App() {
   // Change these numbers to match your template
   const layout = useMemo(() => ({
     x: 370,
-    y: 1120,
+    y: 1110,
     maxWidth: 2,
     startSize: 40,
     minSize: 36,
@@ -103,10 +103,10 @@ export default function App() {
   };
 
   return (
-    <div style={{ maxWidth: 700, margin: "40px auto", padding: 16, fontFamily: "system-ui" }}>
-      <h2>Ramadan Card Generator</h2>
+    <div dir="rtl" style={{ maxWidth: 700, margin: "40px auto", padding: 16, fontFamily: "Tajawal", textAlign: "right" }}>
+      <h2>أهلًا بك في مُصمم البطاقات الرمضانية</h2>
 
-      <label style={{ display: "block", marginBottom: 8 }}>Enter your name</label>
+      <label style={{ display: "block", marginBottom: 8 }}>فضلًا، أدخل اسمك</label>
       <input
         value={name}
         onChange={(e) => setName(e.target.value)}
@@ -118,11 +118,10 @@ export default function App() {
         <canvas ref={canvasRef} style={{ width: "100%", display: "block" }} />
       </div>
 
-      <div style={{ display: "flex", gap: 12, marginTop: 16 }}>
-        <button onClick={downloadPng} style={{ padding: "10px 14px" }}>Download PNG</button>
-        <button onClick={shareImage} style={{ padding: "10px 14px" }}>Share</button>
-        <button class="button" type="button">
-  <span class="button__text">Download</span>
+      <div style={{ display: "flex", gap: 12, marginTop: 16, justifyContent: "center", alignItems: "center",}}>
+        <button onClick={shareImage} class="button" style={{ padding: "10px 14px", borderRadius: "8px"}}><span class="button__text" style={{transform: "translateX(-43px)"}}>مشاركة</span></button>
+        <button class="button" type="button" onClick={downloadPng} style={{ borderRadius: "8px"}}>
+  <span class="button__text" >تنزيل</span>
   <span class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" class="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
 </button>
       </div>
